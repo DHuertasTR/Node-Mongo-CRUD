ca0775ca5a0cecdc8f550f3136eb9df09bfc5583
vardo159 <bramf0@protonmail.com>
2021-11-16 17:59:11 -0500

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import User from '../models/User'
 
-export const signUp = (req,res)=>{
+export const signUp = async (req,res)=>{
+    const {username, email, password, roles}=req.body;
+    console.log(req.body);
     res.json('signup')
 }
 
-export const signIn = (req,res)=>{
+export const signIn =async (req,res)=>{
     res.json('signIn')
 }
