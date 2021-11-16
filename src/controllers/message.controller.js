9c8d6efe092b7c3fb4bd6f1533bd85e0cc2af5d7
david huertas <aletru321@gmail.com>
2021-11-16 03:19:30 -0500

    Create message.controller.js

---
+++ src/controllers/message.controller.js
@@ -0,0 +1,10 @@
+export const createMessage = (req, res) => {
+}
+export const getMessages = (req, res) => {
+}
+export const getMessageById = (req, res) => {
+}
+export const updateMessageById = (req, res) => {
+}
+export const deleteMessageById = (req, res) => {
+}
