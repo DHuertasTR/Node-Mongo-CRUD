bb5d27a6af2d773e3262709af0f3df049742a020
david huertas <aletru321@gmail.com>
2021-11-16 03:14:10 -0500

    message get

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
+import messageRoutes from './routes/message.routes';
+
 const app = express();
 
-app.listen(3000)
+app.use(morgan('dev'));
+
+app.get('/',(req,res)=>{
+    console.log('welcome');
+    res.send('welcome');
+    
+});
+
+app.use('/messages',messageRoutes);
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-
+import app from './app';
 console.log('works');
+app.listen(3000)
