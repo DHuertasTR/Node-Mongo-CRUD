1d4a296c79fc3028cbcd5bd304897ffc60514fc5
vardo159 <bramf0@protonmail.com>
2021-11-16 21:00:11 -0500

    password and token management done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,28 @@
 import User from '../models/User'
+import moduleName from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
+import config from '../config'
 
 export const signUp = async (req,res)=>{
     const {username, email, password, roles}=req.body;
-    console.log(req.body);
-    res.json('signup')
+    
+    const userFound= User.find({email})
+
+    const newUser=new User({
+        username,
+        email,
+        password: await User.encryptPassword(password)  
+    })
+
+    const savedUser=await newUser.save();
+
+    const token=jwt.sign({id:savedUser._id},config.SECRET,{
+        expiresIn:43200
+    })
+
+    res.json({token})
+
+
 }
 
 export const signIn =async (req,res)=>{
