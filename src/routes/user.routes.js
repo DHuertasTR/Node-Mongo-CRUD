a20d905c476007e094b67aabc005a66d38d6ce78
vardo159 <bramf0@protonmail.com>
2021-11-17 15:15:28 -0500

    some user api methods were implemented

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,9 @@
 import {Router} from 'express';
+import * as userC from '../controllers/user.controller'
+import {authJwt} from '../middlewares'
+
 const router = Router();
+
+router.post('/',[authJwt.verifyToken,authJwt.isAdmin],userC.createUser)
 
 export default router;
