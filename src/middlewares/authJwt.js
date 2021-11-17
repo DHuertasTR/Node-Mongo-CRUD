3ac37bbb5f8b855e7a4f475e3da575c3f72da64d
vardo159 <bramf0@protonmail.com>
2021-11-17 11:19:26 -0500

    token verification implemented

---
+++ src/middlewares/authJwt.js
@@ -0,0 +1,25 @@
+import jwt, { decode } from "jsonwebtoken"
+import config from '../config'
+import User from '../models/User'
+
+export const verifyToken=async (req,res,next)=>{
+    try {
+        const token=req.headers["x-access-token"]
+
+        console.log(token)
+
+        if (!token) return res.status(403).json({message: "you don't have access"})
+
+        const decoded=jwt.verify(token,config.SECRET)
+        req.userId = decoded.id
+        
+        const user=await User.findById(req.userId,{password:0})
+        console.log(user)
+
+        if(!user) return res.status(404).json({messsage:"no user found"})
+
+        next()
+    } catch (error) {
+        return res.status(401).json({message:"Unauthorized"})
+    }
+}
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,13 +1,14 @@
 import {Router} from 'express';
 import * as messageController from '../controllers/message.controller';
 import Product from '../models/Message';
+import {verifyToken} from '../middlewares'
 
-const router = Router();
+const router = Router(); 
 
 router.get('/', messageController.getMessages);
-router.post('/', messageController.createMessage);
+router.post('/',verifyToken, messageController.createMessage);
 router.get('/:messageId', messageController.getMessageById);
-router.put('/:messageId', messageController.updateMessageById);
-router.delete('/:messageId', messageController.deleteMessageById);
+router.put('/:messageId',verifyToken, messageController.updateMessageById);
+router.delete('/:messageId',verifyToken, messageController.deleteMessageById);
 
 export default router;
